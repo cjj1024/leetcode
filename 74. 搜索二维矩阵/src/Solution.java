c7d8993e185e6158794ddc608573278ecfeab14f
cjj1024 <xhq021620>
2020-08-14 14:24:25 +0800

    74. 搜索二维矩阵

---
+++ 74. 搜索二维矩阵/src/Solution.java
@@ -0,0 +1,51 @@
+/**
+ * @author cuijunjie
+ * @create 2020--14-11:28
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        };
+        int target = 13;
+
+        System.err.println(searchMatrix(matrix, target));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] > target) {
+                return false;
+            }
+            if (matrix[i][matrix[0].length - 1] < target) {
+            } else {
+                return BinarySearch(matrix[i], target);
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean BinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) >> 1;
+            if (arr[mid] > target) {
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
