db283a9b73d3e300d745ac0bf72b1ba050150567
cjj1024 <xhq021620>
2020-08-17 15:17:39 +0800

    82. 删除排序链表中的重复元素 II

---
+++ 82. 删除排序链表中的重复元素 II/src/Solution.java
@@ -0,0 +1,91 @@
+import com.sun.org.apache.xalan.internal.lib.NodeInfo;
+
+/**
+ * @author cuijunjie
+ * @create 2020--17-9:48
+ */
+public class Solution {
+    public static void main(String[] args) {
+        ListNode dummy = new ListNode(-1);
+        int[] values = {1, 2, 3, 3, 4, 4, 5};
+        ListNode node = dummy;
+        for (int v : values) {
+            node.next = new ListNode(v);
+            node = node.next;
+        }
+        ListNode ret = deleteDuplicates(dummy.next);
+        node = ret;
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null && head.next != null) {
+            if (pre.next.val != head.next.val) {
+                pre = pre.next;
+                head = head.next;
+            }
+            else {
+                while (head.next != null && pre.next.val == head.next.val) {
+                    head = head.next;
+                }
+                pre.next = head.next;
+                head = head.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+//    public static ListNode deleteDuplicates(ListNode head) {
+//        if (head == null || head.next == null) {
+//            return head;
+//        }
+//        ListNode dummy = new ListNode(-1);
+//        ListNode tail = dummy;
+//
+//        ListNode pre = head;
+//        while (head != null) {
+//            while (head != null && pre.val == head.val) {
+//                head = head.next;
+//            }
+//            if (pre.next == head){
+//                tail.next = pre;
+//                tail = tail.next;
+//            }
+//            pre = head;
+//        }
+//        if (tail != null) {
+//            tail.next = null;
+//        }
+//
+//        return dummy.next;
+//    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
