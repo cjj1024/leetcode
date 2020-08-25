564f771d6fa922db595c945755d10fc40583bafa
cjj1024 <xhq021620>
2020-08-25 17:09:21 +0800

    104. 二叉树的最大深度

---
+++ 104. 二叉树的最大深度/src/Solution.java
@@ -0,0 +1,67 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author CuiJunJie
+ * @create 2020-08-25-17:05
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Integer[] nums = {3,9,20,null,null,15,7};
+        MyTreeNode tree = createTree(nums, 0);
+        System.out.println(maxDepth(tree));
+    }
+
+    public static int maxDepth(MyTreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+
+    public static MyTreeNode createTree(Integer[] arr, int i) {
+        if (i >= arr.length || arr[i] == null) {
+            return null;
+        }
+        MyTreeNode root = new MyTreeNode(arr[i]);
+        root.left = createTree(arr, 2 * i + 1);
+        root.right = createTree(arr, 2 * i + 2);
+
+        return root;
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MyTreeNode {
+    int val;
+    MyTreeNode left;
+    MyTreeNode right;
+
+    public MyTreeNode(int val, MyTreeNode left, MyTreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public MyTreeNode(int val) {
+        this.val = val;
+    }
+
+    public MyTreeNode(String val) {
+        this.val = Integer.parseInt(val);
+    }
+}
