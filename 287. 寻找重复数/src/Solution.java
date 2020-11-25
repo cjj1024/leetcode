a74515733219f810a47b8f7777241ddd54b83bab
cjj <2811651110@qq.com>
2020-11-25 14:40:22 +0800

    287. 寻找重复数

--- 287. 寻找重复数/src/Solution.java
+++ 287. 寻找重复数/src/Solution.java
@@ -8,17 +8,42 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(findDuplicate(new int[]{1, 3, 4, 2, 2}));
         System.out.println(findDuplicate(new int[]{3, 1, 3, 4, 2}));
+        System.out.println(findDuplicate(new int[]{1, 1}));
     }
 
     public static int findDuplicate(int[] nums) {
-        BitSet bitSet = new BitSet();
-        for (int num : nums) {
-            if (bitSet.get(num)) {
-                return num;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        int ret = -1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) {
+                    count++;
+                }
             }
-            bitSet.set(num);
+            if (count <= mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                ret = mid;
+            }
         }
 
-        return -1;
+        return ret;
     }
+
+//    public static int findDuplicate(int[] nums) {
+//        BitSet bitSet = new BitSet();
+//        for (int num : nums) {
+//            if (bitSet.get(num)) {
+//                return num;
+//            }
+//            bitSet.set(num);
+//        }
+//
+//        return -1;
+//    }
 }
