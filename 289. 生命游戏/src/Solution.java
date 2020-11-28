e63179964a7c326a613a940db384d64abec6d5ec
cjj <2811651110@qq.com>
2020-11-28 16:50:23 +0800

    289. 生命游戏

--- 289. 生命游戏/src/Solution.java
+++ 289. 生命游戏/src/Solution.java
@@ -21,6 +21,55 @@ public class Solution {
     }
 
     public static void gameOfLife(int[][] board) {
+        if (board == null || board.length < 1 || board[0].length < 1) {
+            return;
+        }
 
+        int[][] newBoard = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int num = 0;
+                if (i - 1 >= 0) {
+                    num += board[i - 1][j];
+                    if (j - 1 >= 0) {
+                        num += board[i - 1][j - 1];
+                    }
+                    if (j + 1 < board[0].length) {
+                        num += board[i - 1][j + 1];
+                    }
+                }
+                if (i + 1 < board.length) {
+                    num += board[i + 1][j];
+                    if (j - 1 >= 0) {
+                        num += board[i + 1][j - 1];
+                    }
+                    if (j + 1 < board[0].length) {
+                        num += board[i + 1][j + 1];
+                    }
+                }
+                if (j - 1 >= 0) {
+                    num += board[i][j - 1];
+                }
+                if (j + 1 < board[0].length) {
+                    num += board[i][j + 1];
+                }
+
+                if (num < 2) {
+                    newBoard[i][j] = 0;
+                } else if (num == 2 && board[i][j] == 1) {
+                    newBoard[i][j] = 1;
+                } else if (num == 3) {
+                    newBoard[i][j] = 1;
+                } else if (num > 3) {
+                    newBoard[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = newBoard[i][j];
+            }
+        }
     }
 }
