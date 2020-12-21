2d0f22595bb38e5905253bdf01dfa954ccc87cfc
cjj <2811651110@qq.com>
2020-12-21 11:24:51 +0800

    746. 使用最小花费爬楼梯

---
+++ 746. 使用最小花费爬楼梯/src/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * @author CuiJunJie
+ * @create 2020/12/21-11:19
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        System.out.println(minCostClimbingStairs(new int[]{0, 0, 0, 1}));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+
+        return Math.min(dp[cost.length - 2], dp[cost.length - 1]);
+    }
+}
