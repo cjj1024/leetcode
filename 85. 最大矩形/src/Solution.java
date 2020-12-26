693c16b6294c1504df7b1c2079007f35dffc2de9
cjj <2811651110@qq.com>
2020-12-26 11:32:28 +0800

    85. 最大矩形

--- 85. 最大矩形/src/Solution.java
+++ 85. 最大矩形/src/Solution.java
@@ -23,42 +23,83 @@ public class Solution {
             return 0;
         }
 
-        int[] left = new int[matrix[0].length];
-        int[] right = new int[matrix[0].length];
-        Stack<Integer> stack = new Stack<>();
-        int[] heights = new int[matrix[0].length];
-        int maxArea = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] heights = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == '1') {
+                heights[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0;j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    heights[j] += 1;
-                } else {
-                    heights[j] = 0;
+                    heights[i][j] = heights[i - 1][j] + 1;
                 }
             }
-            stack.clear();
-            for (int j = 0; j < matrix[0].length; j++) {
-                while (!stack.isEmpty() && heights[stack.peek()] >= heights[j]) {
-                    stack.pop();
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int width = 0;
+                int height = Integer.MAX_VALUE;
+                for (int k = j; k < n; k++) {
+                    if (heights[i][k] == 0) {
+                        break;
+                    } else {
+                        width++;
+                        height = Math.min(heights[i][k], height);
+                        maxArea = Math.max(maxArea, width * height);
+                    }
                 }
-                left[j] = stack.isEmpty() ? -1 : stack.peek();
-                stack.push(j);
-            }
-            stack.clear();
-            for (int j = matrix[0].length - 1; j >= 0; j--) {
-                while (!stack.isEmpty() && heights[stack.peek()] >= heights[j]) {
-                    stack.pop();
-                }
-                right[j] = stack.isEmpty() ? heights.length : stack.peek();
-                stack.push(j);
-            }
-            for (int j = 0; j < matrix[0].length; j++) {
-                maxArea = Math.max(maxArea, heights[j] * (right[j] - left[j] - 1));
             }
         }
 
         return maxArea;
     }
+
+//    public static int maximalRectangle(char[][] matrix) {
+//        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+//            return 0;
+//        }
+//
+//        int[] left = new int[matrix[0].length];
+//        int[] right = new int[matrix[0].length];
+//        Stack<Integer> stack = new Stack<>();
+//        int[] heights = new int[matrix[0].length];
+//        int maxArea = 0;
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[0].length; j++) {
+//                if (matrix[i][j] == '1') {
+//                    heights[j] += 1;
+//                } else {
+//                    heights[j] = 0;
+//                }
+//            }
+//            stack.clear();
+//            for (int j = 0; j < matrix[0].length; j++) {
+//                while (!stack.isEmpty() && heights[stack.peek()] >= heights[j]) {
+//                    stack.pop();
+//                }
+//                left[j] = stack.isEmpty() ? -1 : stack.peek();
+//                stack.push(j);
+//            }
+//            stack.clear();
+//            for (int j = matrix[0].length - 1; j >= 0; j--) {
+//                while (!stack.isEmpty() && heights[stack.peek()] >= heights[j]) {
+//                    stack.pop();
+//                }
+//                right[j] = stack.isEmpty() ? heights.length : stack.peek();
+//                stack.push(j);
+//            }
+//            for (int j = 0; j < matrix[0].length; j++) {
+//                maxArea = Math.max(maxArea, heights[j] * (right[j] - left[j] - 1));
+//            }
+//        }
+//
+//        return maxArea;
+//    }
 
 //    public static int maximalRectangle(char[][] matrix) {
 //        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
