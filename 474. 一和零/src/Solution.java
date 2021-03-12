502fdd84a21f13073dcb40011472c748c5650c89
cjj <2811651110@qq.com>
2021-03-12 11:05:14 +0800

    474. 一和零

--- 474. 一和零/src/Solution.java
+++ 474. 一和零/src/Solution.java
@@ -12,49 +12,25 @@ public class Solution {
     }
 
     public static int findMaxForm(String[] strs, int m, int n) {
-        int[] num0 = new int[strs.length];
-        int[] num1 = new int[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = 0; j < strs[i].length(); j++) {
-                if (strs[i].charAt(j) == '0') {
-                    num0[i]++;
+        int[][] dp =  new int[m + 1][n + 1];
+        for (String str : strs) {
+            int zeroNum = 0;
+            int oneNum = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '0') {
+                    zeroNum++;
                 } else {
-                    num1[i]++;
+                    oneNum++;
+                }
+            }
+
+            for (int i = m; i >= zeroNum; i--) {
+                for (int j = n; j >= oneNum; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
                 }
             }
         }
 
-        int[] existNum0 = new int[strs.length];
-        int[] existNum1 = new int[strs.length];
-        int[] dp = new int[strs.length];
-
-
-        for (int i = 0; i < strs.length; i++) {
-            if (num0[i] <= m && num1[i] <= n) {
-                existNum0[i] = num0[i];
-                existNum1[i] = num1[i];
-                dp[i] = 1;
-            }
-        }
-
-        for (int i = 1; i < strs.length; i++) {
-            int index = -1;
-            int minValue = -1;
-            for (int j = 0; j < i; j++) {
-                if (existNum0[j] + num0[i] <= m && existNum1[j] + num1[i] <= n) {
-                    if (dp[j] > minValue) {
-                        index = j;
-                        minValue = dp[j];
-                    }
-                }
-            }
-            if (index != -1) {
-                existNum0[i] = existNum0[index] + num0[i];
-                existNum1[i] = existNum1[index] + num1[i];
-                dp[i] = dp[index] + 1;
-            }
-        }
-
-        return Arrays.stream(dp).max().getAsInt();
+        return dp[m][n];
     }
 }
