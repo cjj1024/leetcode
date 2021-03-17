95a3d1f99b7ed9a42e2e335ea21fb6474f3031d2
cjj <2811651110@qq.com>
2021-03-17 10:44:25 +0800

    115. 不同的子序列

--- 115. 不同的子序列/src/Solution.java
+++ 115. 不同的子序列/src/Solution.java
@@ -9,38 +9,61 @@ public class Solution {
     }
 
     public static int numDistinct(String s, String t) {
-        if (s == null && t == null) {
-            return 1;
-        }
-        if (s == null || t == null) {
-            return 0;
-        }
         if (s.length() < t.length()) {
             return 0;
         }
 
-        int[][] dp = new int[t.length() + 1][s.length() + 1];
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
         for (int i = 0; i <= s.length(); i++) {
-            dp[0][i] = 1;
-        }
-        for (int i = 1; i < t.length(); i++) {
-            dp[i][0] = 0;
+            dp[i][0] = 1;
         }
 
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-        for (int j = 1; j <= sChar.length; j++) {
-            for (int i = 1; i <= tChar.length; i++) {
-                if (sChar[j - 1] == tChar[i - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
-                    dp[i][j] = dp[i][j - 1];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return dp[tChar.length][sChar.length];
+        return dp[s.length()][t.length()];
     }
+
+//    public static int numDistinct(String s, String t) {
+//        if (s == null && t == null) {
+//            return 1;
+//        }
+//        if (s == null || t == null) {
+//            return 0;
+//        }
+//        if (s.length() < t.length()) {
+//            return 0;
+//        }
+//
+//        int[][] dp = new int[t.length() + 1][s.length() + 1];
+//        for (int i = 0; i <= s.length(); i++) {
+//            dp[0][i] = 1;
+//        }
+//        for (int i = 1; i < t.length(); i++) {
+//            dp[i][0] = 0;
+//        }
+//
+//        char[] sChar = s.toCharArray();
+//        char[] tChar = t.toCharArray();
+//        for (int j = 1; j <= sChar.length; j++) {
+//            for (int i = 1; i <= tChar.length; i++) {
+//                if (sChar[j - 1] == tChar[i - 1]) {
+//                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+//                } else {
+//                    dp[i][j] = dp[i][j - 1];
+//                }
+//            }
+//        }
+//
+//        return dp[tChar.length][sChar.length];
+//    }
 
 //    public static int numDistinct(String s, String t) {
 //        if (s == null && t == null) {
