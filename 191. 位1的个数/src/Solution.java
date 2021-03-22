e8369a2c0b7e94dbb443a18c89d9d9da831bea58
cjj <2811651110@qq.com>
2021-03-22 09:35:36 +0800

    191. 位1的个数

--- 191. 位1的个数/src/Solution.java
+++ 191. 位1的个数/src/Solution.java
@@ -5,7 +5,19 @@
 public class Solution {
     public static void main(String[] args) {
         int n = 13;
-        System.out.println(hammingWeight(n));
+        System.out.println(hammingWeight2(n));
+    }
+
+    public static int hammingWeight2(int n) {
+        int num = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n + 1) % 2 == 0) {
+                num++;
+            }
+            n = n >>> 1;
+        }
+
+        return num;
     }
 
     public static int hammingWeight(int n) {
