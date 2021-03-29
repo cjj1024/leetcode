f206e5f2ab6e628963410bd1a2d6f884785a1b2e
cjj <2811651110@qq.com>
2021-03-29 10:10:58 +0800

    190. 颠倒二进制位

--- 190. 颠倒二进制位/src/Solution.java
+++ 190. 颠倒二进制位/src/Solution.java
@@ -4,11 +4,31 @@
  */
 public class Solution {
     public static void main(String[] args) {
-        int n = 1;
-        System.out.println(reverseBits(n));
+
+//        System.out.println(reverseBits(43261596)); //00000010100101000001111010011100 964176192
+        System.out.println(reverseBits(-3)); //11111111111111111111111111111101 -1073741825
+        System.out.println(Integer.toBinaryString(-3));
     }
 
     public static int reverseBits(int n) {
+        int ret = 0;
+
+        if (n % 2 == 1 || n % 2 == -1) {
+            ret = ret | 1;
+        }
+        for (int i = 0; i < 31; i++) {
+            ret = ret << 1;
+            n = n >> 1;
+
+            if (n % 2 == 1 || n % 2 == -1) {
+                ret = ret | 1;
+            }
+        }
+
+        return ret;
+    }
+
+    public static int reverseBits2(int n) {
         n = (n >>> 16) | (n << 16);
         n = ((n & 0b11111111000000001111111100000000) >>> 8) | ((n & 0b00000000111111110000000011111111) << 8);
         n = ((n & 0b11110000111100001111000011110000) >>> 4) | ((n & 0b00001111000011110000111100001111) << 4);
--- 730. 统计不同回文子序列/src/Solution.java
+++ 730. 统计不同回文子序列/src/Solution.java
@@ -1,13 +1,75 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @author CuiJunJie
  * @create 2021/3/12-16:10
  */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(countPalindromicSubsequences("abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba"));
+        int[] arr = new int[50000];
+        Random random = new Random();
+        for (int i = 0; i < 50000; i++) {
+            arr[i] = i;
+        }
+        long start = System.currentTimeMillis();
+        Arrays.sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+//        System.out.println(countPalindromicSubsequences("bccb"));    // 6
+//        System.out.println(countPalindromicSubsequences("abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba"));
     }
 
     public static int countPalindromicSubsequences(String S) {
+        return 0;
+    }
 
+    public static int countPalindromicSubsequences2(String S) {
+        char[] chars = S.toCharArray();
+
+        return backtrace(chars, 0, new StringBuilder(S.length()));
+    }
+
+    private static Set<String> set = new HashSet<>();
+    private static int backtrace(char[] chars, int i, StringBuilder sb) {
+        int num = 0;
+        if (isPalindrom(sb)) {
+            if (!set.contains(sb.toString())) {
+                num++;
+                System.out.println(sb);
+                set.add(sb.toString());
+            }
+        }
+
+        if (i == chars.length) {
+            return num;
+        }
+
+        sb.append(chars[i]);
+        num += backtrace(chars, i + 1, sb);
+        sb.deleteCharAt(sb.length() - 1);
+        num += backtrace(chars, i + 1, sb);
+
+        return num;
+    }
+
+    private static boolean isPalindrom(StringBuilder sb) {
+        if (sb.length() < 1) {
+            return false;
+        }
+        if (sb.length() == 1) {
+            return true;
+        }
+        int i = 0;
+        int j = sb.length() - 1;
+        while (i < j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
