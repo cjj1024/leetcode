f601d6ead2716b7308e01b558cd1f4df4c845fbd
cjj1024 <2811651110@qq.com>
2021-03-31 19:20:59 +0800

    90. 子集 II

---
+++ 1562. 查找大小为 M 的最新分组/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * @author CuiJunJie
+ * @create 2021/3/30-17:12
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(findLatestStep(new int[]{3, 5, 1, 2, 4}, 1));
+    }
+
+    public static int findLatestStep(int[] arr, int m) {
+        
+    }
+}
--- 90. 子集 II/src/Solution.java
+++ 90. 子集 II/src/Solution.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author cuijunjie
@@ -8,7 +6,7 @@ import java.util.List;
  */
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
+        int[] nums = {1, 2, 2};
         List<List<Integer>> ret = subsetsWithDup(nums);
         for (List<Integer> item : ret) {
             for (Integer x : item) {
@@ -16,43 +14,115 @@ public class Solution {
             }
             System.out.println();
         }
-//        [[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3],[3,2]]
-//        [[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]]
     }
 
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ret = new ArrayList<>();
-        ret.add(new ArrayList<>());
-        if (nums == null || nums.length < 1) {
-            return ret;
-        }
         Arrays.sort(nums);
-
-        boolean[] used = new boolean[nums.length];
-        ArrayList<Integer> tmp = new ArrayList<>(nums.length);
-        backtrace(nums, 0, 0, used, ret, tmp);
+        List<List<Integer>> ret = new ArrayList<>();
+        backtrace(nums, ret, new ArrayList<>(), 0);
 
         return ret;
     }
 
-    public static void backtrace(int[] nums, int k, int len, boolean[] used, List<List<Integer>> ret, List<Integer> tmp) {
-        if (len == nums.length) {
-            return;
-        }
-
-        for (int i = k; i < nums.length; i++) {
-            if (used[i]) {
+    private static void backtrace(int[] nums, List<List<Integer>> ret, ArrayList<Integer> list, int start) {
+        ret.add(new ArrayList<>(list));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
                 continue;
             }
-            if (i > 0 && !used[i - 1] && nums[i - 1] == nums[i]) {
-                continue;
-            }
-            used[i] = true;
-            tmp.add(nums[i]);
-            ret.add(new ArrayList<>(tmp));
-            backtrace(nums, i + 1, len + 1, used, ret, tmp);
-            tmp.remove(len);
-            used[i] = false;
+            list.add(nums[i]);
+            backtrace(nums, ret, list, i + 1);
+            list.remove(list.size() - 1);
         }
     }
+
+//    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+//        Arrays.sort(nums);
+//
+//        List<List<Integer>> ret = new ArrayList<>();
+//        backtrace(ret, nums, 0, new ArrayList<>(), false);
+//
+//        return ret;
+//    }
+//
+//    public static void backtrace(List<List<Integer>> ret, int[] nums, int k, List<Integer> list, boolean used) {
+//        if (k == nums.length) {
+//            ret.add(new ArrayList<>(list));
+//            return;
+//        }
+//
+//        if (k > 0 && nums[k] == nums[k - 1] && !used) {
+//            backtrace(ret, nums, k + 1, list, false);
+//        } else {
+//            list.add(nums[k]);
+//            backtrace(ret, nums, k + 1, list, true);
+//            list.remove(list.size() - 1);
+//            backtrace(ret, nums, k + 1, list, false);
+//        }
+//    }
+
+
+
+//    public List<List<Integer>> subsetsWithDup(int[] nums) {
+//        Arrays.sort(nums);
+//
+//        Map<String, List<Integer>> map = new HashMap<>();
+//        backtrace(map, nums, 0, new ArrayList<>());
+//
+//        List<List<Integer>> ret = new ArrayList<>();
+//        for (String s : map.keySet()) {
+//            ret.add(map.get(s));
+//        }
+//
+//        return ret;
+//    }
+//
+//    public void backtrace(Map<String, List<Integer>> ret, int[] nums, int k, List<Integer> list) {
+//        if (k == nums.length) {
+//            ret.put(list.toString(), new ArrayList<>(list));
+//            return;
+//        }
+//
+//        list.add(nums[k]);
+//        backtrace(ret, nums, k + 1, list);
+//        list.remove(list.size() - 1);
+//        backtrace(ret, nums, k + 1, list);
+//
+//    }
+//
+//    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+//        List<List<Integer>> ret = new ArrayList<>();
+//        ret.add(new ArrayList<>());
+//        if (nums == null || nums.length < 1) {
+//            return ret;
+//        }
+//        Arrays.sort(nums);
+//
+//        boolean[] used = new boolean[nums.length];
+//        ArrayList<Integer> tmp = new ArrayList<>(nums.length);
+//        backtrace(nums, 0, 0, used, ret, tmp);
+//
+//        return ret;
+//    }
+//
+//    public static void backtrace(int[] nums, int k, int len, boolean[] used, List<List<Integer>> ret, List<Integer> tmp) {
+//        if (len == nums.length) {
+//            return;
+//        }
+//
+//        for (int i = k; i < nums.length; i++) {
+//            if (used[i]) {
+//                continue;
+//            }
+//            if (i > 0 && !used[i - 1] && nums[i - 1] == nums[i]) {
+//                continue;
+//            }
+//            used[i] = true;
+//            tmp.add(nums[i]);
+//            ret.add(new ArrayList<>(tmp));
+//            backtrace(nums, i + 1, len + 1, used, ret, tmp);
+//            tmp.remove(len);
+//            used[i] = false;
+//        }
+//    }
 }
