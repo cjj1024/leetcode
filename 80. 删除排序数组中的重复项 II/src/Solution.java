35449e57f42eebaf5eda57291b9be28ade7f8038
cjj1024 <2811651110@qq.com>
2021-04-06 09:43:57 +0800

    80. 删除排序数组中的重复项 II

--- 80. 删除排序数组中的重复项 II/src/Solution.java
+++ 80. 删除排序数组中的重复项 II/src/Solution.java
@@ -4,28 +4,50 @@
  */
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,2,2,3};
+        int[] nums = {0,0,1,1,1,1,2,3,3};
         int num = removeDuplicates(nums);
         System.out.println(num);
     }
 
     public static int removeDuplicates(int[] nums) {
-        if (nums.length < 2) {
-            return nums.length;
-        }
-        int count = 1;
-        int k = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[k] == nums[i]) {
-                count++;
-            } else {
-                count = 1;
-            }
-            if (count <= 2) {
-                nums[++k] = nums[i];
-            }
+        if (nums == null || nums.length < 1) {
+            return 0;
         }
 
-        return ++k;
+        int n = 1;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                n++;
+            } else {
+                n = 1;
+            }
+            if (n > 2) {
+                continue;
+            }
+            nums[k++] = nums[i];
+        }
+
+        return k;
     }
+
+//    public static int removeDuplicates(int[] nums) {
+//        if (nums.length < 2) {
+//            return nums.length;
+//        }
+//        int count = 1;
+//        int k = 0;
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[k] == nums[i]) {
+//                count++;
+//            } else {
+//                count = 1;
+//            }
+//            if (count <= 2) {
+//                nums[++k] = nums[i];
+//            }
+//        }
+//
+//        return ++k;
+//    }
 }
