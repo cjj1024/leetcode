0ee40e0ef8a7bda27dcd210144855de5b049d45c
cjj1024 <2811651110@qq.com>
2021-04-06 09:19:29 +0800

    88. 合并两个有序数组

--- 88. 合并两个有序数组/src/Solution.java
+++ 88. 合并两个有序数组/src/Solution.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  */
 public class Solution {
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums1 = {1,2,3,0,0, 0};
         int[] nums2 = {2, 5, 6};
         merge(nums1, 3, nums2, nums2.length);
         for (int i = 0; i < nums1.length; i++) {
@@ -14,23 +14,47 @@ public class Solution {
         }
     }
 
+
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1, j = n - 1;
-        int k = nums1.length - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        System.arraycopy(nums1, 0, nums1, nums1.length - m, m);
+        int i = nums1.length - m;
+        int j = 0;
+        int k = 0;
+        while (i < m + n && j < n) {
+            if (nums1[i] < nums2[j]) {
+                nums1[k++] = nums1[i++];
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[k++] = nums2[j++];
             }
         }
-        while (i >= 0) {
-            nums1[k--] = nums1[i--];
+        while (i < m + n) {
+            nums1[k++] = nums1[i++];
         }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        while (j < n) {
+            nums1[k++] = nums2[j++];
         }
     }
+
+
+
+
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int i = m - 1, j = n - 1;
+//        int k = nums1.length - 1;
+//        while (i >= 0 && j >= 0) {
+//            if (nums1[i] > nums2[j]) {
+//                nums1[k--] = nums1[i--];
+//            } else {
+//                nums1[k--] = nums2[j--];
+//            }
+//        }
+//        while (i >= 0) {
+//            nums1[k--] = nums1[i--];
+//        }
+//        while (j >= 0) {
+//            nums1[k--] = nums2[j--];
+//        }
+//    }
 
 
 //    public static void merge(int[] nums1, int m, int[] nums2, int n) {
