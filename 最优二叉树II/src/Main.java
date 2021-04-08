3d676f937df9b29e57c5b4aab6ccc6f2c6694788
cjj1024 <2811651110@qq.com>
2021-04-08 15:39:53 +0800

    最优二叉树II

--- 最优二叉树II/src/Main.java
+++ 最优二叉树II/src/Main.java
@@ -18,37 +18,54 @@ public class Main {
     private static int[] values;
     private static int minValue = Integer.MAX_VALUE;
     private static Node root;
+    private static int[][][] dp;
 
 
     public static void readInput() throws IOException {
         reader = new BufferedReader(new InputStreamReader(System.in));
         writer = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(reader.readLine());
-        values = new int[N];
-        int i = 0;
+        values = new int[N + 1];
+        int i = 1;
         for (String s : reader.readLine().split(" ")) {
             values[i++] = Integer.parseInt(s);
         }
+        dp = new int[N + 1][N + 1][N + 1];
+        for (i = 0; i < N + 1; i++) {
+            for (int j = 0; j < N + 1; j++) {
+                for (int k = 0; k < N + 1; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         readInput();
-        buildTree(0);
+
+        int minValue = recursion(1, N, 0);
+
+        writer.write(String.valueOf(minValue));
+        writer.flush();
     }
 
-    private static Node buildTree(int node) {
-        if (node == N) {
-            minValue = Math.min(minValue, getValues());
-            return null;
+    private static int recursion(int left, int right, int parent) {
+        if (left > right) {
+            return 0;
         }
-        if (node < 0) {
-            return null;
+        if (dp[left][right][parent] != -1) {
+            return dp[left][right][parent];
         }
 
+        int minValue = Integer.MAX_VALUE;
+        for (int i = left; i <= right; i++) {
+            int leftValue = recursion(left, i - 1, i);
+            int rightValue = recursion(i + 1, right, i);
+            minValue = Math.min(minValue, leftValue + rightValue + values[i] * values[parent]);
+        }
 
-    }
+        dp[left][right][parent] = minValue;
 
-    private static int getValues() {
-        return 0;
+        return minValue;
     }
 }
