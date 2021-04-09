11c277e243b01cde4de29df51fab27e2b2421ce0
cjj1024 <2811651110@qq.com>
2021-04-09 10:54:21 +0800

    154. 寻找旋转排序数组中的最小值 II

--- 154. 寻找旋转排序数组中的最小值 II/src/Solution.java
+++ 154. 寻找旋转排序数组中的最小值 II/src/Solution.java
@@ -6,23 +6,45 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {2,2,2,0,1};
         System.out.println(findMin(nums));
+        System.out.println(findMin(new int[]{1, 3, 5}));
     }
 
     public static int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        int mid;
-        while (left < right) {
-            mid = (left + right) >> 1;
-            if (nums[mid] < nums[right]) {
-                right = mid;
-            } else if (nums[mid] > nums[right]) {
+        int mid = (right - left) / 2 + left;
+        while (left <= right) {
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+            mid = (right - left) / 2 + left;
+            if (nums[mid] > nums[left]) {
                 left = mid + 1;
+            } else if (nums[mid] < nums[left]) {
+                right = mid;
             } else {
-                right -= 1;
+                left++;
             }
         }
 
-        return nums[left];
+        return nums[right];
     }
+
+//    public static int findMin(int[] nums) {
+//        int left = 0;
+//        int right = nums.length - 1;
+//        int mid;
+//        while (left < right) {
+//            mid = (left + right) >> 1;
+//            if (nums[mid] < nums[right]) {
+//                right = mid;
+//            } else if (nums[mid] > nums[right]) {
+//                left = mid + 1;
+//            } else {
+//                right -= 1;
+//            }
+//        }
+//
+//        return nums[left];
+//    }
 }
