564a66aaf87cd1e93066eec4e36fa822179fad17
cjj1024 <2811651110@qq.com>
2021-04-19 09:46:41 +0800

    220. 存在重复元素 III

--- 220. 存在重复元素 III/src/Solution.java
+++ 220. 存在重复元素 III/src/Solution.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author CuiJunJie
@@ -8,36 +6,74 @@ import java.util.TreeSet;
  */
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {2147483640,2147483641};
-        int k = 1, t = 100;
+//        int[] nums = {2147483640,2147483641};
+//        int k = 1, t = 100;
+//
+//        System.out.println(containsNearbyAlmostDuplicate(nums, k, t));
 
-        System.out.println(containsNearbyAlmostDuplicate(nums, k, t));
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,2,3,1}, 3, 0));  // true
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,0,1,1}, 1, 2)); // true
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,5,9,1,5,9}, 2, 3));  // false
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{-2147483648,2147483647}, 1, 1));  // false
     }
 
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null) {
-            return false;
-        }
-
-        TreeSet<Integer> set = new TreeSet<>();
+        TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            Integer floor = set.floor(nums[i]);
-            if (floor != null && nums[i] <= (long) floor + t) {
+            Long c = set.ceiling((long) nums[i]);
+            Long f = set.floor((long) nums[i]);
+            if (c != null && c - nums[i] <= t) {
                 return true;
             }
-            Integer ceiling = set.ceiling(nums[i]);
-            if (ceiling != null && nums[i] + t >= (long)  ceiling) {
+            if (f != null && nums[i] - f <= t) {
                 return true;
             }
-
-            set.add(nums[i]);
+            set.add((long) nums[i]);
             if (set.size() > k) {
-                set.remove(nums[i - k]);
+                set.remove((long) nums[i - k]);
             }
         }
 
         return false;
     }
+
+//    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length && j - i <= k; j++) {
+//                if (Math.abs((long) nums[i] - nums[j]) <= t) {
+//                    return true;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
+
+//    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+//        if (nums == null) {
+//            return false;
+//        }
+//
+//        TreeSet<Integer> set = new TreeSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            Integer floor = set.floor(nums[i]);
+//            if (floor != null && nums[i] <= (long) floor + t) {
+//                return true;
+//            }
+//            Integer ceiling = set.ceiling(nums[i]);
+//            if (ceiling != null && nums[i] + t >= (long)  ceiling) {
+//                return true;
+//            }
+//
+//            set.add(nums[i]);
+//            if (set.size() > k) {
+//                set.remove(nums[i - k]);
+//            }
+//        }
+//
+//        return false;
+//    }
 
 
 //    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
--- 87. 扰乱字符串/src/Solution.java
+++ 87. 扰乱字符串/src/Solution.java
@@ -18,62 +18,65 @@ public class Solution {
         System.out.println(isScramble("ccababcaabcb", "bccbccaaabab")); // true
     }
 
-    public static boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        char[] cstr1 = s1.toCharArray();
-        char[] cstr2 = s2.toCharArray();
-        if (isEqual(Arrays.copyOfRange(cstr1, 0, cstr1.length), Arrays.copyOfRange(cstr2, 0, cstr2.length)) == 0) {
-            return false;
-        }
 
-        return helper(cstr1, cstr2, 0, cstr1.length - 1, 0, cstr2.length - 1);
-    }
 
-    public static int isEqual(char[] cstr1, char[] cstr2) {
-        int ret = 0;
-        if (String.valueOf(cstr1).equals(String.valueOf(cstr2))) {
-            ret += 1;
-        }
 
-        Arrays.sort(cstr1);
-        Arrays.sort(cstr2);
-        if (String.valueOf(cstr1).equals(String.valueOf(cstr2))) {
-            ret += 1;
-        }
-        return ret;
-    }
-
-    private static boolean helper(char[] cstr1, char[] cstr2, int start1, int end1, int start2, int end2) {
-        if (start1 > end1 || start2 > end2) {
-            return false;
-        }
-
-        if (start1 == end1) {
-            return cstr1[start1] == cstr2[start2];
-        }
-
-        for (int i = start1, j = start2; i < end1 && j < end2; i++, j++) {
-            int flag;
-            if ((flag = isEqual(Arrays.copyOfRange(cstr1, start1, i + 1),
-                    Arrays.copyOfRange(cstr2, start2, j + 1))) > 0) {
-                if (helper(cstr1, cstr2, start1, i, start2, j)
-                        && helper(cstr1, cstr2, i + 1, end1, j + 1, end2)) {
-                    return true;
-                }
-            }
-            if ((flag = isEqual(Arrays.copyOfRange(cstr1, start1, i + 1),
-                    Arrays.copyOfRange(cstr2, end2 - j + start2, end2 + 1))) > 0) {
-                if (helper(cstr1, cstr2, start1, i, end2 - j + start2, end2)
-                        && helper(cstr1, cstr2, i + 1, end1, start2, end2 - j + start2 - 1)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
+//    public static boolean isScramble(String s1, String s2) {
+//        if (s1.length() != s2.length()) {
+//            return false;
+//        }
+//        char[] cstr1 = s1.toCharArray();
+//        char[] cstr2 = s2.toCharArray();
+//        if (isEqual(Arrays.copyOfRange(cstr1, 0, cstr1.length), Arrays.copyOfRange(cstr2, 0, cstr2.length)) == 0) {
+//            return false;
+//        }
+//
+//        return helper(cstr1, cstr2, 0, cstr1.length - 1, 0, cstr2.length - 1);
+//    }
+//
+//    public static int isEqual(char[] cstr1, char[] cstr2) {
+//        int ret = 0;
+//        if (String.valueOf(cstr1).equals(String.valueOf(cstr2))) {
+//            ret += 1;
+//        }
+//
+//        Arrays.sort(cstr1);
+//        Arrays.sort(cstr2);
+//        if (String.valueOf(cstr1).equals(String.valueOf(cstr2))) {
+//            ret += 1;
+//        }
+//        return ret;
+//    }
+//
+//    private static boolean helper(char[] cstr1, char[] cstr2, int start1, int end1, int start2, int end2) {
+//        if (start1 > end1 || start2 > end2) {
+//            return false;
+//        }
+//
+//        if (start1 == end1) {
+//            return cstr1[start1] == cstr2[start2];
+//        }
+//
+//        for (int i = start1, j = start2; i < end1 && j < end2; i++, j++) {
+//            int flag;
+//            if ((flag = isEqual(Arrays.copyOfRange(cstr1, start1, i + 1),
+//                    Arrays.copyOfRange(cstr2, start2, j + 1))) > 0) {
+//                if (helper(cstr1, cstr2, start1, i, start2, j)
+//                        && helper(cstr1, cstr2, i + 1, end1, j + 1, end2)) {
+//                    return true;
+//                }
+//            }
+//            if ((flag = isEqual(Arrays.copyOfRange(cstr1, start1, i + 1),
+//                    Arrays.copyOfRange(cstr2, end2 - j + start2, end2 + 1))) > 0) {
+//                if (helper(cstr1, cstr2, start1, i, end2 - j + start2, end2)
+//                        && helper(cstr1, cstr2, i + 1, end1, start2, end2 - j + start2 - 1)) {
+//                    return true;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
 
 //    public static boolean isScramble(String s1, String s2) {
 //        if (s1 == null && s2 == null) {
@@ -104,43 +107,37 @@ public class Solution {
 //        return false;
 //    }
 
-//    public static boolean isScramble(String s1, String s2) {
-//        if (s1 == null && s2 == null) {
-//            return true;
-//        }
-//        if (s1 == null && s2 != null || s1 != null && s2 == null) {
-//            return false;
-//        }
-//        if (s1.length() != s2.length()) {
-//            return false;
-//        }
-//
-//        char[] sChar1 = s1.toCharArray();
-//        char[] sChar2 = s2.toCharArray();
-//        int n = sChar1.length;;
-//        boolean[][][] dp = new boolean[n][n][n + 1];
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                dp[i][j][1] = sChar1[i] == sChar2[j];
-//            }
-//        }
-//        for (int k = 2; k <= n; k++) {
-//            for (int i = 0; i <= n - k; i++) {
-//                for (int j = 0; j <= n - k; j++) {
-//                    for (int w = 1; w < k; w++) {
-//                        if (dp[i][j][w] && dp[i + w][j + w][k - w]) {
-//                            dp[i][j][k] = true;
-//                            break;
-//                        }
-//                        if (dp[i][j + k - w][w] && dp[i + w][j][k - w]) {
-//                            dp[i][j][k] = true;
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        return dp[0][0][n];
-//    }
+    public static boolean isScramble(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] sChar1 = s1.toCharArray();
+        char[] sChar2 = s2.toCharArray();
+        int n = sChar1.length;;
+        boolean[][][] dp = new boolean[n][n][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][1] = sChar1[i] == sChar2[j];
+            }
+        }
+        for (int k = 2; k <= n; k++) {
+            for (int i = 0; i <= n - k; i++) {
+                for (int j = 0; j <= n - k; j++) {
+                    for (int w = 1; w < k; w++) {
+                        if (dp[i][j][w] && dp[i + w][j + w][k - w]) {
+                            dp[i][j][k] = true;
+                            break;
+                        }
+                        if (dp[i][j + k - w][w] && dp[i + w][j][k - w]) {
+                            dp[i][j][k] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][n];
+    }
 }
