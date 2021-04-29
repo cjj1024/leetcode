6983493eb24b0534cc75b718101a4bcf559d0ce4
cjj1024 <2811651110@qq.com>
2021-04-29 15:58:27 +0800

    403. 青蛙过河

--- 403. 青蛙过河/src/Solution.java
+++ 403. 青蛙过河/src/Solution.java
@@ -15,33 +15,60 @@ public class Solution {
 
     private static Map<String, Boolean> map;
     public static boolean canCross(int[] stones) {
-        map = new HashMap<>();
-        return recursion(stones, 0, 0);
+//        map = new HashMap<>();
+//        return recursion(stones, 0, 0);
+
+        int len = stones.length;
+        boolean[][] dp = new boolean[len][len];
+        dp[0][0] = true;
+        for (int i = 1; i < len; i++) {
+            if (stones[i] - stones[i - 1] > i) {
+                return false;
+            }
+        }
+        for (int i = 1; i < len; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                int k = stones[i] - stones[j];
+                if (k > j + 1) {
+                    break;
+                }
+                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (dp[len - 1][i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public static boolean recursion(int[] stones, int k, int steps) {
-        String key = k + "#" + steps;
-        if (map.containsKey(key)) {
-            return map.get(key);
-        }
-        if (k == stones.length - 1) {
-            return true;
-        }
 
-        boolean flag = false;
-        for (int i = k + 1; i < stones.length; i++) {
-            if (stones[k] + steps - 1 == stones[i]) {
-                flag |= recursion(stones, i, steps - 1);
-            }
-            if (stones[k] + steps == stones[i]) {
-                flag |= recursion(stones, i, steps);
-            }
-            if (stones[k] + steps + 1 == stones[i]) {
-                flag |= recursion(stones, i, steps + 1);
-            }
-        }
-        map.put(key, flag);
 
-        return flag;
-    }
+//    public static boolean recursion(int[] stones, int k, int steps) {
+//        String key = k + "#" + steps;
+//        if (map.containsKey(key)) {
+//            return map.get(key);
+//        }
+//        if (k == stones.length - 1) {
+//            return true;
+//        }
+//
+//        boolean flag = false;
+//        for (int i = k + 1; i < stones.length; i++) {
+//            if (stones[k] + steps - 1 == stones[i]) {
+//                flag |= recursion(stones, i, steps - 1);
+//            }
+//            if (stones[k] + steps == stones[i]) {
+//                flag |= recursion(stones, i, steps);
+//            }
+//            if (stones[k] + steps + 1 == stones[i]) {
+//                flag |= recursion(stones, i, steps + 1);
+//            }
+//        }
+//        map.put(key, flag);
+//
+//        return flag;
+//    }
 }
