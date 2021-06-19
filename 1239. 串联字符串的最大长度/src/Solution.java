e282f3ac9d15a4805285ffb3b92133ccf5e12657
cjj1024 <2811651110@qq.com>
2021-06-19 09:57:13 +0800

    1239. 串联字符串的最大长度

--- 1239. 串联字符串的最大长度/src/Solution.java
+++ 1239. 串联字符串的最大长度/src/Solution.java
@@ -15,62 +15,98 @@ public class Solution {
         System.out.println(maxLength(Arrays.asList("yy","bkhwmpbiisbldzknpm")));   // 0
     }
 
-    private static Set<Character> set;
     private static int maxLen = 0;
     public static int maxLength(List<String> arr) {
-        List<String> newArr = arr.stream().filter(s -> {
-            set = new HashSet<>();
+        List<Integer> masks = arr.stream().map(s -> {
+            int mask = 0;
+            int ch = 0;
             for (char c : s.toCharArray()) {
-                if (set.contains(c)) {
-                    return false;
+                ch = c - 'a';
+                if (((mask >> ch) & 1) == 1) {
+                    return 0;
                 }
-                set.add(c);
+                mask |= (1 << ch);
             }
-            return true;
+            return mask;
+        }).filter(mask -> {
+            return mask > 0;
         }).collect(Collectors.toList());
 
-        set = new HashSet<>();
         maxLen = 0;
 
-        backtrace(newArr, 0);
+        backtrace(masks, 0, 0);
 
         return maxLen;
     }
 
-    private static void backtrace(List<String> arr, int k) {
-        if (k == arr.size()) {
-            maxLen = Math.max(maxLen, set.size());
+    private static void backtrace(List<Integer> masks, Integer mask, int k) {
+        if (k == masks.size()) {
+            maxLen = Math.max(maxLen, Integer.bitCount(mask));
             return;
         }
 
-        if (!isContain(arr.get(k))) {
-            addToS(arr.get(k));
-            backtrace(arr, k + 1);
-            removeFromS(arr.get(k));
+        if ((mask & masks.get(k)) == 0) {
+            backtrace(masks, mask | masks.get(k), k + 1);
         }
-
-        backtrace(arr, k + 1);
+        backtrace(masks, mask, k + 1);
     }
 
-    private static void removeFromS(String s) {
-        for (char c : s.toCharArray()) {
-            set.remove(c);
-        }
-    }
-
-    private static void addToS(String s) {
-        for (char c : s.toCharArray()) {
-            set.add(c);
-        }
-    }
-
-    private static boolean isContain(String s) {
-        for (char c : s.toCharArray()) {
-            if (set.contains(c)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
+//    private static Set<Character> set;
+//    private static int maxLen = 0;
+//    public static int maxLength(List<String> arr) {
+//        List<String> newArr = arr.stream().filter(s -> {
+//            set = new HashSet<>();
+//            for (char c : s.toCharArray()) {
+//                if (set.contains(c)) {
+//                    return false;
+//                }
+//                set.add(c);
+//            }
+//            return true;
+//        }).collect(Collectors.toList());
+//
+//        set = new HashSet<>();
+//        maxLen = 0;
+//
+//        backtrace(newArr, 0);
+//
+//        return maxLen;
+//    }
+//
+//    private static void backtrace(List<String> arr, int k) {
+//        if (k == arr.size()) {
+//            maxLen = Math.max(maxLen, set.size());
+//            return;
+//        }
+//
+//        if (!isContain(arr.get(k))) {
+//            addToS(arr.get(k));
+//            backtrace(arr, k + 1);
+//            removeFromS(arr.get(k));
+//        }
+//
+//        backtrace(arr, k + 1);
+//    }
+//
+//    private static void removeFromS(String s) {
+//        for (char c : s.toCharArray()) {
+//            set.remove(c);
+//        }
+//    }
+//
+//    private static void addToS(String s) {
+//        for (char c : s.toCharArray()) {
+//            set.add(c);
+//        }
+//    }
+//
+//    private static boolean isContain(String s) {
+//        for (char c : s.toCharArray()) {
+//            if (set.contains(c)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
 }
