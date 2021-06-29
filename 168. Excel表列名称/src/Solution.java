0bedc7b62416d823d8cbd479a36a9e6f40f03997
cjj1024 <2811651110@qq.com>
2021-06-29 14:39:01 +0800

    168. Excel表列名称

--- 168. Excel表列名称/src/Solution.java
+++ 168. Excel表列名称/src/Solution.java
@@ -4,24 +4,38 @@
  */
 public class Solution {
     public static void main(String[] args) {
-        int n = 701;
+        int n = 701;        // ZY
         System.out.println(convertToTitle(n));
+        System.out.println(convertToTitle(2147483647));         // FXSHRXW
     }
 
-    public static String convertToTitle(int n) {
-        if (n < 1) {
-            return "";
+    private static char[] number = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber != 0) {
+            columnNumber--;
+            int x = columnNumber % 26;
+            sb.append(number[x]);
+            columnNumber = columnNumber / 26;
         }
 
-        StringBuilder ret = new StringBuilder();
-        while (n != 0) {
-            n--;
-            int num = n % 26;
-            ret.append((char) (num + 'A'));
-            n = n / 26;
-
-        }
-
-        return ret.reverse().toString();
+        return sb.reverse().toString();
     }
+
+//    public static String convertToTitle(int n) {
+//        if (n < 1) {
+//            return "";
+//        }
+//
+//        StringBuilder ret = new StringBuilder();
+//        while (n != 0) {
+//            n--;
+//            int num = n % 26;
+//            ret.append((char) (num + 'A'));
+//            n = n / 26;
+//
+//        }
+//
+//        return ret.reverse().toString();
+//    }
 }
