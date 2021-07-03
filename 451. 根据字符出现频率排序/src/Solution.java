b36bc289e268fa4e75ff48819a5e55aa4bdaf7b2
cjj1024 <2811651110@qq.com>
2021-07-03 10:02:24 +0800

    451. 根据字符出现频率排序

---
+++ 451. 根据字符出现频率排序/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author CuiJunJie
+ * @create 2021/7/3-10:01
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static String frequencySort(String s) {
+        Character[] sChars = new Character[s.length()];
+        Map<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            sChars[i++] = c;
+        }
+
+        Arrays.sort(sChars, (c1, c2) -> {
+            if (map.get(c1).equals(map.get(c2))) {
+                return (int) ((int) c1 - c2);
+            } else {
+                return map.get(c2) - map.get(c1);
+            }
+        });
+
+        char[] t = new char[s.length()];
+        for (i = 0; i < t.length; i++) {
+            t[i] = sChars[i];
+        }
+        return String.valueOf(t);
+    }
+}
